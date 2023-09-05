d28b7371d5a03b9cb98ccb56e62766887c223fc7
Ayoub <contact@maatouch.com>
2023-09-05 21:57:35 +0100

    added grouping

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,26 +1,86 @@
 
 <template>
-    <div class=" mx-auto justify-center">
+    <div class="mx-auto justify-center">
+    
+
+        <h1> {{ptPer}} </h1>
+        <div class="w-4/6 mx-auto">
+            <div class="bg-blue-200 h-3.5 dark:bg-blue-400">
+
+                <div class="bg-green-400 h-3.5 " :style="{width: ptPer + '%'}"></div>
+            </div>
+        </div>
         <div class="mx-auto" ref="chart"></div>
+        <v-divider :thickness="10"></v-divider>
+        
+        
+        <!-- print a green square with the text me next to it-->
+        <div class="flex justify-begin">
+            <strong>Abbreviation:</strong>    
+        </div>
+        <div class="flex justify-begin">
+            <div class="square bg-green-400" style="; width: 1%; padding-bottom: 1%;"></div>
+            <div> PT </div> 
+        </div>
+        
+        <v-divider :thickness="20" class="border-opacity-0"></v-divider>
+        <div class="flex justify-begin">
+            <div class="square dark:bg-blue-400" style="width: 1%; padding-bottom: 1%;"></div> 
+            <div class="mx-50">CT</div>
+        </div>
+        <v-divider :thickness="20" class="border-opacity-0"></v-divider>
+    
+        
     </div>
 </template>
 
 <script>
 import vegaEmbed from 'vega-embed';
 var DATA = []
+var abbreviation = []
+var ptPer = 0;
 export default {
 
     props: ['data'],
     setup(props) {
         DATA = props.data
 
+
     },
     mounted() {
         this.renderChart();
+        this.get_value();
     },
     methods: {
+        get_value() {
+            abbreviation = [...new Array(DATA.map((item) => item.abbreviation))]
+            console.log(abbreviation)
+            console.log(abbreviation.length)
+
+            if (abbreviation[0].length > 0) {
+
+                var total = abbreviation[0].length;
+                var ptCount = 0;
+                var stCount = 0;
+                for (let i = 0; i < total; i++) {
+                   if (abbreviation[0][i] === 'PT') {
+                        ptCount++;
+                     } else {
+                     stCount++;
+                    }
+    }
+    
+                const ptPercentage = Math.round((ptCount / total) * 100);
+                this.ptPer = ptPercentage;
+
+                return ptPercentage;
+            }
+            
+
+        },
         async renderChart() {
             console.log("prps data ", DATA)
+            console.log("ptPer ", ptPer)
             const spec = {
                 "title": "Visualization of Gene Expression data",
                 "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
--- src/components/Heatmap.vue
+++ src/components/Heatmap.vue
@@ -84,6 +84,9 @@
         <h1 class="mb-4 text-3xl  text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Heatmap</span>
         </h1>
+
+          <div class="d-flex rounded-lg">
+          </div>
         <chart :key="reload" :data="data" />
         </div>
 
@@ -101,6 +104,8 @@
     </v-col>
     </v-row>
   </v-responsive>
+
+  
 </template>
 
 <script setup>
@@ -151,4 +156,5 @@ export default {
 
 
 </script>
-<style src="@vueform/multiselect/themes/default.css"></style>
+<style src="@vueform/multiselect/themes/default.css">
+</style>
